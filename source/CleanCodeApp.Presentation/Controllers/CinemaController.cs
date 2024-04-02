ef8890f7dd0fa2f689efe9d7ad27696cd673824e
rachanee-tw <rachanee.saengkrajai@jetstar.com>
2024-04-02 12:32:45 +0700

    Fix routing issue

--- source/CleanCodeApp.Presentation/Controllers/CinemaController.cs
+++ source/CleanCodeApp.Presentation/Controllers/CinemaController.cs
@@ -7,39 +7,43 @@ using CleanCodeApp.Presentation.DTOs;
 namespace CleanCodeApp.Presentation.Controllers;
 
 [ApiController]
-[Route("[controller]")]
 public class CinemaController(CinemaService cinemaService) : ControllerBase
 {
     private readonly CinemaService _cinemaService = cinemaService;
 
-    [HttpGet(Name = "GetNowShowingMovies")]
+    [HttpGet]
+    [Route("GetNowShowingMovies")]
     public IEnumerable<MovieDto> GetNowShowingMovies()
     {
         var nowShowingMovies = _cinemaService.GetNowShowingMovies();
         return nowShowingMovies.Select(m => m.ToDto());
     }
 
-    [HttpGet(Name = "SearchMoviesByTitle")]
+    [HttpGet]
+    [Route("SearchMoviesByTitle/{title}")]
     public IEnumerable<MovieDto> SearchMoviesByTitle([FromRoute] string title)
     {
         var movies = _cinemaService.SearchMoviesByTitle(title);
         return movies.Select(m => m.ToDto());
     }
 
-    [HttpGet(Name = "GetMovieDetails")]
+    [HttpGet]
+    [Route("GetMovieDetails/{movieId}")]
     public MovieDto GetMovieDetails([FromRoute] Guid movieId)
     {
         return _cinemaService.GetMovieDetails(movieId).ToDto();
     }
 
-    [HttpGet(Name = "GetShowTimesByMovieId")]
+    [HttpGet]
+    [Route("GetShowTimesByMovieId/{movieId}")]
     public IEnumerable<ShowTimeDto> GetShowTimesByMovieId([FromRoute] Guid movieId)
     {
         var showTimes = _cinemaService.GetShowTimesByMovieId(movieId);
         return showTimes.Select(s => s.ToDto());
     }
 
-    [HttpPost(Name = "CreateBooking")]
+    [HttpPost]
+    [Route("CreateBooking")]
     public BookingDto CreateBooking([FromBody] CreateBookingRequestModel requestModel)
     {
         var booking = _cinemaService.CreateBooking(requestModel.ShowtimeId, requestModel.selectedSeatKeys, requestModel.CustomerAccountId);
